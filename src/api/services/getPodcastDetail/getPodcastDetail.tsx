c3ab3bf87cb0db25ff92b597572f7b0f634e1029
DiegoEstela <die.estela@gmail.com>
2023-03-13 04:04:09 +0100

    test cors

--- src/api/services/getPodcastDetail/getPodcastDetail.tsx
+++ src/api/services/getPodcastDetail/getPodcastDetail.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
 import { IPodcastDetailList } from "../../../app/global/interfaces";
 import { iTunesPath } from "../../../app/global/endpoints";
 
@@ -7,7 +7,13 @@ export async function getPodcastDetail(id: string): Promise<AxiosResponse<IPodca
     try {
         const path: string = `${iTunesPath}/lookup?id=${id}&media=podcast
         &entity=podcastEpisode&limit=20`
-        const { data, status } = await axios.get(path, { withCredentials: false });
+        const headers: AxiosRequestConfig = {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            }
+        }
+        const { data, status } = await axios.get(path, headers);
         console.log("response status is: ", status);
         return data
     } catch (error) {
